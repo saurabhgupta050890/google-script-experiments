305b8e0d8445598943c2d478555346a0739a71f7
Saurabh Gupta <saurabhgupta0508@gmail.com>
2023-01-02 03:05:46 +0530

    mutual fund stock distrubution

---
+++ moneycontrol-mf-details/index.ts
@@ -0,0 +1,141 @@
+interface IMoneyControlSearchSuggestion {
+  pdt_dis_nm: string;
+  link_src: string;
+  link_track: string;
+}
+
+interface IMutualFundStock {
+  name: string;
+  percentage: number;
+  scrip?: string;
+  sector?: string;
+  type?: string;
+}
+
+const suggest1 = (data: IMoneyControlSearchSuggestion[]) => data;
+
+const getPortfolioUrl = (url: string) => {
+  const baseUrl = url.replace("/nav", "");
+  const lastSlash = baseUrl.lastIndexOf("/");
+  return (
+    baseUrl.substring(0, lastSlash) +
+    "/portfolio-holdings" +
+    baseUrl.substring(lastSlash)
+  );
+};
+
+const tableToDistribution = (
+  table: any,
+  keys: Record<number, string>,
+  $: any
+): IMutualFundStock[] => {
+  const tableRows = table.find("tbody").last().find("tr");
+  const columns = Object.keys(keys).map((key) => parseInt(key, 10));
+  const distribution: IMutualFundStock[] = [];
+  tableRows.each((index, el) => {
+    // @ts-ignore
+    const row = $(el);
+    const distributionObj = {};
+    columns.forEach((val) => {
+      distributionObj[keys[val]] = row
+        .find("td")
+        .eq(val)
+        .text()
+        .trim()
+        .replace("%", "");
+    });
+    distribution.push(distributionObj as IMutualFundStock);
+  });
+
+  return distribution.map((d) => {
+    return {
+      ...d,
+      percentage: Number(d.percentage),
+    };
+  });
+};
+
+const extractMututalFundDistribution = (
+  fund: IMoneyControlSearchSuggestion
+) => {
+  const portfolioUrl = getPortfolioUrl(fund.link_src);
+  const resp = UrlFetchApp.fetch(portfolioUrl, { muteHttpExceptions: true });
+  // @ts-ignore
+  const $ = Cheerio.load(resp.getContentText());
+  const equityTable = $("#equityCompleteHoldingTable");
+  const debtTable = $("#portfolioDebtTable");
+  const othersTable = $("table.portf_others");
+
+  const equityKeys = { 0: "name", 1: "sector", 4: "percentage" };
+  const equityDistribution = tableToDistribution(equityTable, equityKeys, $);
+
+  const debtKeys = { 0: "name", 1: "type", 2: "sector", 6: "percentage" };
+  const debtDistribution = tableToDistribution(debtTable, debtKeys, $);
+
+  const otherKeys = { 0: "name", 1: "type", 2: "percentage" };
+  const otherDistribution = tableToDistribution(othersTable, otherKeys, $);
+
+  const sumPercent = (arr: IMutualFundStock[]) =>
+    arr.reduce((acc, cur) => Number((acc + cur.percentage).toFixed(2)), 0);
+
+  return {
+    equity: {
+      percentage: sumPercent(equityDistribution),
+      distribution: equityDistribution,
+    },
+    debt: {
+      percentage: sumPercent(debtDistribution),
+      distribution: debtDistribution,
+    },
+    others: {
+      percentage: sumPercent(otherDistribution),
+      distribution: otherDistribution,
+    },
+  };
+};
+
+const extractMututalFundDetails = (name: string) => {
+  const moneycontrolSearchUrl = `https://www.moneycontrol.com/mccode/common/autosuggestion_solr.php?classic=true&query=${encodeURIComponent(
+    name
+  )}&type=2&format=json&callback=suggest1`;
+  const response = UrlFetchApp.fetch(moneycontrolSearchUrl, {
+    muteHttpExceptions: true,
+  });
+  const suggestions: IMoneyControlSearchSuggestion[] = eval(
+    response.getContentText()
+  );
+
+  const nearestMatchSuggestion = suggestions.reduce((previous, current) => {
+    // @ts-ignore
+    const preMatch = StringSimilarity.stringSimilarity(
+      previous.pdt_dis_nm,
+      name
+    );
+    // @ts-ignore
+    const curMatch = StringSimilarity.stringSimilarity(
+      current.pdt_dis_nm,
+      name
+    );
+
+    return preMatch > curMatch ? previous : current;
+  });
+
+  // Logger.log(nearestMatchSuggestion);
+  return nearestMatchSuggestion;
+};
+
+const prepareMutualFundMaster = () => {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
+  const data = sheet.getDataRange().getValues();
+  const jsonColumnIndex = data[0].indexOf("JSON");
+
+  for (let i = 1; i < data.length; i++) {
+    const [mutualFundName, isin, amfi, lastUpdated] = data[i];
+    const mf = extractMututalFundDetails(mutualFundName);
+    const mfDistribution = extractMututalFundDistribution(mf);
+    sheet.getRange(i + 1, jsonColumnIndex).setValue(new Date().toISOString());
+    sheet
+      .getRange(i + 1, jsonColumnIndex + 1)
+      .setValue(JSON.stringify(mfDistribution));
+  }
+};
